5c1b0a6abf50c8eb80aa719397079cb5a1713dcd
Harsh-Prajapati6114 <prajapatiharsh6114@gmail.com>
2026-04-13 18:17:06 +0530

    update to dev

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 //const PORT = 3000;
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('This is staging environment');
+    res.send('This is staging environment!!');
     res.send('🚀 Hello DevOps Engineer Harsh!');
 });
 
